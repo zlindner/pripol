370bc1b3ebd3d625e19a8a1dfdfbf1fd8db44574
zlindner <zach.lindner@hotmail.com>
2019-05-30 21:27:12 -0400

    moved opp115.py to data layer, updated vectors.py with vector paths

--- layers/ml/vectors.py
+++ layers/ml/vectors.py
@@ -1,21 +1,21 @@
-from gensim.models import KeyedVectors as kv
 import numpy as np
+from gensim.models import KeyedVectors as kv
 
 # loads acl1010 vectors
 def load_acl1010():
 	print('\n# loading acl1010 vectors')
-	vectors = kv.load('vectors/acl1010.vec', mmap='r')
+	vectors = kv.load('../data/acl1010/acl1010.vec', mmap='r')
 	print('loaded %s vectors' % (len(vectors.wv.vocab)))
 	return vectors
 
 # loads google news vectors
 def load_google():
 	print('\n# loading google news vectors')
-	vectors = kv.load_word2vec_format('vectors/google-news.bin', binary=True)
+	vectors = kv.load_word2vec_format('../data/google/google-news.bin', binary=True)
 	print('loaded %s vectors' % (len(vectors.vocab)))
 	return vectors
 
-# builds the embedding matrix from vec
+# builds an embedding matrix from word2vec
 def build_embedding_matrix(vectors, dim):
 	vocab = vectors.wv.vocab
 	vocab_size = len(vectors.wv.vocab) + 1
