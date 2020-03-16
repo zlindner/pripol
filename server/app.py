6926e681864c7f8bd0a682637476def705b2fbb2
zlindner <zach.lindner@hotmail.com>
2020-03-15 22:16:24 -0400

    server now runs with built react fronted

--- server/app.py
+++ server/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__, static_folder='../dist', template_folder='../dist')
+    app = Flask(__name__, template_folder='../dist', static_folder='../dist/js')
 
     register_routes(app)
 
     return app
 
 def register_routes(app):
+    @app.route('/', defaults={'path': ''})
     @app.route('/<path:path>')
     def serve(path):
         return render_template('index.html')
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
